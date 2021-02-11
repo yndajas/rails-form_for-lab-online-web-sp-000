e3517ac267eda6d43cc2feeba685bd0481a3a7dd
Ynda Jas <yndajas@gmail.com>
2021-02-11 06:59:05 +0000

    commit tree 6a8490b5b5d864d23b3302776eeb16b5a7a993e8

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :students, only: [:show, :new, :create, :edit]
+  patch 'students/:id', to: 'students#update'
+  resources :'school_classes', only: [:show, :new, :create, :edit], :path => '/school-classes'
+  patch 'school-classes/:id', to: 'school_classes#update'\
 end
